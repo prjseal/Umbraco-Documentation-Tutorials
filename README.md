2ccfd50caf4bd767c2ab12cabb6084e52f978a2f
prjseal <prjseal@gmail.com>
2018-09-25 23:39:22 +0100

    - first tutorial creating a property editor

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Umbraco-Documentation-Tutorials
 A project for me to follow along with the tutorials in the Umbraco Documentation
+
+## Current Completed Tutorials
+
+1. [Creating a Property Editor](https://our.umbraco.com/documentation/Tutorials/Creating-a-Property-Editor/)
---
+++ UDT.Web/App_Plugins/MarkDownEditor/markdowneditor.controller.js
@@ -0,0 +1,23 @@
+﻿angular.module("umbraco")
+    .controller("My.MarkdownEditorController",
+        // inject umbracos assetsService
+        function ($scope, assetsService) {
+
+            // tell the assetsService to load the markdown.editor libs from the markdown editors
+            // plugin folder
+            assetsService
+                .load([
+                    "~/App_Plugins/MarkDownEditor/lib/markdown.converter.js",
+                    "~/App_Plugins/MarkDownEditor/lib/markdown.sanitizer.js",
+                    "~/App_Plugins/MarkDownEditor/lib/markdown.editor.js"
+                ])
+                .then(function () {
+                    // this function will execute when all dependencies have loaded
+                    var converter2 = new Markdown.Converter();
+                    var editor2 = new Markdown.Editor(converter2, "-" + $scope.model.alias);
+                    editor2.run();
+                });
+
+            // load the separate css for the editor to avoid it blocking our js loading
+            assetsService.loadCss("~/App_Plugins/MarkDownEditor/lib/markdown.css");
+        });
