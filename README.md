ca3f3be59d7c237972c6153df9e030bdd104f2f2
prjseal <prjseal@gmail.com>
2018-09-26 23:28:45 +0100

    - added EditorModelEventHandler.cs

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ A project for me to follow along with the tutorials in the Umbraco Documentation
 ## Current Completed Tutorials
 
 1. [Creating a Property Editor](https://our.umbraco.com/documentation/Tutorials/Creating-a-Property-Editor/)
+
+2. [EditorModel Events](https://our.umbraco.com/documentation/reference/events/EditorModel-Events)
---
+++ UDT.Web/EventHandlers/EditorModelEventHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Umbraco.Core;
+using Umbraco.Web.Editors;
+
+namespace UDT.Web.EventHandlers
+{
+    public class EditorModelEventHandler : ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            //Here we are binding the new method we created below to the SendingContentModel event.
+            EditorModelEventManager.SendingContentModel += EditorModelEventManager_SendingContentModel;
+        }
+
+        /// <summary>
+        /// This method adds some default values to the model properties when the SendingContentModel event is fired.
+        /// </summary>
+        private void EditorModelEventManager_SendingContentModel(System.Web.Http.Filters.HttpActionExecutedContext sender, 
+            EditorModelEventArgs<Umbraco.Web.Models.ContentEditing.ContentItemDisplay> e)
+        {
+            if (e.Model.ContentTypeAlias == "blogpost")
+            {
+                var articleDate = e.Model.Properties.FirstOrDefault(x => x.Alias == "articleDate");
+                if (articleDate != null && (articleDate.Value == null || string.IsNullOrWhiteSpace(articleDate.Value.ToString())))
+                {
+                    articleDate.Value = DateTime.Now.Date;
+                }
+            }
+        }
+    }
+}
