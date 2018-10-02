d63944d264f18289f100cb1fb6e92bf1cb9ec78c
prjseal <prjseal@gmail.com>
2018-10-02 23:51:48 +0100

    - added youtube cover image data type

---
+++ UDT.Web/App_Plugins/YouTubeCoverImage/lib/ytci.js
@@ -0,0 +1,29 @@
+﻿var YTCI = YTCI || {
+    init: function () {
+        YTCI.addListeners();
+    },
+    addListeners: function () {
+        $('#ytci-get-image').on('click', function (e) {
+            var button = $(this);
+            e.preventDefault();
+            var textbox = $(button).prev();
+            var url = textbox.val();
+            var videoId = '';
+            var qs = url.split('?')[1];
+            var sURLVariables = qs.split('&');
+            for (var i = 0; i < sURLVariables.length; i++)
+            {
+                var sParameterName = sURLVariables[i].split('=');
+                if (sParameterName[0] == 'v')
+                {
+                    videoId = sParameterName[1];
+                }
+            }
+
+            var imageUrl = 'https://i.ytimg.com/vi/' + videoId + '/hqdefault.jpg';
+            $(textbox).val(imageUrl);
+            var image = $(button).next();
+            $(image).attr('src', imageUrl);
+        });
+    }
+}
---
+++ UDT.Web/App_Plugins/YouTubeCoverImage/youtubecoverimage.controller.js
@@ -0,0 +1,18 @@
+﻿angular.module("umbraco")
+    .controller("YouTubeCoverImageController",
+        // inject umbracos assetsService
+        function ($scope, assetsService) {
+
+            // tell the assetsService to load the ytci libs from the plugin folder
+            assetsService
+                .load([
+                    "~/App_Plugins/YouTubeCoverImage/lib/ytci.js"
+                ])
+                .then(function () {
+                    // this function will execute when all dependencies have loaded
+                    YTCI.init();
+                });
+
+            // load the separate css for the editor to avoid it blocking our js loading
+            assetsService.loadCss("~/App_Plugins/YouTubeCoverImage/lib/ytci.css");
+        });
